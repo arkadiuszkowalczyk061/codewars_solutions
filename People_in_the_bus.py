734e530dc3b514d0819e6b97a3593a9cf75ae0f3
Arkadiusz Kowalczyk <akowalczyk061@gmail.com>
2020-03-30 11:37:04 +0200

    Added People_in_the_bus.py

---
+++ People_in_the_bus.py
@@ -0,0 +1,16 @@
+#There is a bus moving in the city, and it takes and drop some people in each bus stop.
+#
+# You are provided with a list (or array) of integer arrays (or tuples). Each integer array has two items which represent number of people get into bus (The first item) and number of people get off the bus (The second item) in a bus stop.
+#
+# Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+
+GETTING_INTO_THE_BUS = 0
+LEAVING = 1
+
+def number(bus_stops):
+    sleepingPeople = 0
+    for stop in bus_stops:
+        sleepingPeople += stop[GETTING_INTO_THE_BUS] - stop[LEAVING]
+    return sleepingPeople
+
+print(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]))
