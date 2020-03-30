015186278c72f80b79ad468466e8241a92ea4cd3
Arkadiusz Kowalczyk <akowalczyk061@gmail.com>
2020-03-30 11:41:08 +0200

    Renaming global variables

--- People_in_the_bus.py
+++ People_in_the_bus.py
@@ -4,13 +4,13 @@
 #
 # Your task is to return number of people who are still in the bus after the last bus station (after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
 
-GETTING_INTO_THE_BUS = 0
-LEAVING = 1
+PEOPLE_IN = 0
+PEOPLE_OUT = 1
 
 def number(bus_stops):
     sleepingPeople = 0
     for stop in bus_stops:
-        sleepingPeople += stop[GETTING_INTO_THE_BUS] - stop[LEAVING]
+        sleepingPeople += stop[PEOPLE_IN] - stop[PEOPLE_OUT]
     return sleepingPeople
 
 print(number([[3,0],[9,1],[4,8],[12,2],[6,1],[7,8]]))
