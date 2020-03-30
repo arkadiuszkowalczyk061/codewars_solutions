b9abc197df4d2b2488fcc794e73adc3dc5822263
Arkadiusz Kowalczyk <akowalczyk061@gmail.com>
2020-03-30 10:37:51 +0200

    Added task details

--- is_a_square.py
+++ is_a_square.py
@@ -1,5 +1,12 @@
 """This method checks if the given number is a perfect square"""
 
+# Task
+# Given an integral number, determine if it's a square number:
+#
+# In mathematics, a square number or perfect square is an integer that is the square of an integer; in other words, it is the product of some integer with itself.
+#
+# The tests will always use some integral number, so don't worry about that in dynamic typed languages.
+
 def is_square(n):
     if n >= 0:
         if int(n**0.5)** 2 == n:
--- replace_with_alphabet.py
+++ replace_with_alphabet.py
@@ -1,5 +1,15 @@
 """This method replaces letters in text with position in alphabet"""
 
+#In this kata you are required to, given a string, replace every letter with its position in the alphabet.
+#
+# If anything in the text isn't a letter, ignore it and don't return it.
+#
+# "a" = 1, "b" = 2, etc.
+#
+# Example
+# alphabet_position("The sunset sets at twelve o' clock.")
+# Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 1 20 20 23 5 12 22 5 15 3 12 15 3 11" (as a string)
+
 def alphabet_position(text):
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
     text = list(text)
